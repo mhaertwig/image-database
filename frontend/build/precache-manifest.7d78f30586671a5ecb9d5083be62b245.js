948c4761c613a8d9f624630a97c4cc39114026e7
Mario Härtwig <mario.haertwig@geops.de>
2019-06-16 00:00:25 +0200

    Update frontend build.

---
+++ frontend/build/precache-manifest.7d78f30586671a5ecb9d5083be62b245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "683ea38ae0d07d8e83a808a3cf705b49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b7ad48cd761fd0882b9",
+    "url": "/static/css/main.a3f2addc.chunk.css"
+  },
+  {
+    "revision": "b215702e21d2d02a5eac",
+    "url": "/static/js/2.1039360a.chunk.js"
+  },
+  {
+    "revision": "3b7ad48cd761fd0882b9",
+    "url": "/static/js/main.cdbe2e58.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
