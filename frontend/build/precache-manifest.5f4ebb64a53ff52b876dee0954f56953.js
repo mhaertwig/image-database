8bc7cc2d8314f9669ff58acaea6b33d020cfe488
Mario Härtwig <mario.haertwig@geops.de>
2019-06-29 20:22:10 +0200

    Update build.

---
+++ frontend/build/precache-manifest.5f4ebb64a53ff52b876dee0954f56953.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f700f1ceda93b5d9fb7a1e1d124242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44e39887c66a957628ff",
+    "url": "/static/css/main.ba3e6739.chunk.css"
+  },
+  {
+    "revision": "1b5dfd829a91daeb62c1",
+    "url": "/static/js/2.01fc8a0d.chunk.js"
+  },
+  {
+    "revision": "44e39887c66a957628ff",
+    "url": "/static/js/main.1846b9c5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
